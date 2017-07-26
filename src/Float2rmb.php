66f32fa22cab871a071375806a6a9cd38bd5c3eb
zhangzhijie <aigoudebook@gmail.com>
2017-07-26 09:32:08 +0000

    project added

---
+++ src/Float2rmb.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * @数字金额转人民币大写
+ * Created by PhpStorm.
+ * Author: Baykier<1035666345@qq.com>
+ * Date: 2017/7/26
+ * Time: 17:10
+ */
+namespace Baykier\Float2rmb;
+
+class Float2rmb
+{
+    /**
+     * @数字对应中文
+     * @var array
+     */
+    protected static $c = array(
+        '0' => '零',
+        '1' => '壹',
+        '2' => '贰',
+        '3' => '叁',
+        '4' => '肆',
+        '5' => '伍',
+        '6' => '陆',
+        '7' => '柒',
+        '8' => '捌',
+        '9' => '玖',
+    );
+    /**
+     * @log 对数 字典
+     * @var array
+     */
+    protected static $l = array(
+        '-2' => '分',
+        '-1' => '角',
+        '0' => '元',
+        '1' => '拾',
+        '2' => '佰',
+        '3' => '仟',
+        '4' => '万',
+        '5' => '拾',
+        '6' => '佰',
+        '7' => '仟',
+        '8' => '亿',
+        '9' => '拾',
+        '10' => '佰',
+        '11' => '仟',
+        '12' => '万',
+        '13' => '拾',
+        '14' => '佰',
+        '15' => '仟',
+    );
+
+    /**
+     * @转换
+     * @param $string
+     */
+    public static function convert($string)
+    {
+        self::check();
+        $rmb = '';
+        $loop = true;
+        $base = '0.01';
+
+        while ($loop) {
+            $t = bcdiv($string,$base,0);
+            if ($t >= 1) {
+                $b = substr($t,-1);
+                if ($b >= 1) {
+                    $lS = (int) log10($base);
+                    $rmb = self::$c[$b] . self::$l[$lS] . $rmb;
+                }else
+                {
+                    $rmb = (mb_substr($rmb,0,1) == '零') ? $rmb : '零' . $rmb;
+                }
+            }else
+            {
+                $loop = false;
+            }
+            $base = $base * 10;
+        }
+        if (mb_substr($rmb,-1) == '零') {
+            $rmb = mb_substr($rmb,0,mb_strlen($rmb) -1) . '整';
+        }
+        return $rmb;
+    }
+
+    /**
+     * @检查依赖是否安装
+     * @return bool
+     * @throws \Exception
+     */
+    protected static function check()
+    {
+        if (!extension_loaded('bcmath'))
+        {
+            throw new \Exception('请先安装bcmath扩展');
+        }
+        if (!extension_loaded('mbstring')) {
+            throw new \Exception('请先安装mbstring扩展');
+        }
+        return true;
+    }
+
+}
